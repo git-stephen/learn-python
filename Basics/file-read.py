34707a718760237991874070d19cf3216409b5ad
stephen <stephen280290@gmail.com>
2024-05-04 13:25:11 +0530

    first commit

---
+++ Basics/file-read.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+
+data = pd.read_csv("C:/Users/steph/source/repos/Python Code/data/email.csv")
+
+print('============================')
+print(data.head(5))
+print(('============================'))
